6a8ecc877326f6ef424dd35c592135bba702336a
Vaibhav-Pawar00 <vjpawr05@gmail.com>
2025-10-14 21:41:06 +0530

    ADDED R script file Practical 1

---
+++ Practical1.r
@@ -0,0 +1,23 @@
+#Print a message
+print("Hello This is text message")
+
+x <- 10
+y <- 5
+
+#Assignment Operator
+sum_value <- x+y
+diff_value <- x-y
+prod_value <- x*y
+div_value <- x/y
+
+sum_value
+diff_value
+prod_value
+div_value
+
+
+# Relational Operator
+greater_check <- x>y
+equal_check <- x==y
+
+
